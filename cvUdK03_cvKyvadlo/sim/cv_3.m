41c5fd0db2532305cdeb109fe901281404a53b93
Martin Dodek <dodekm1@gmail.com>
2020-10-08 16:39:10 +0200

    cv3

--- cvUdK03_cvKyvadlo/sim/cv_3.m
+++ cvUdK03_cvKyvadlo/sim/cv_3.m
@@ -12,7 +12,7 @@ beta=0.5*sqrt(g/l); %koeficient viskózneho trenia
 
 %poèiatoèné podmienky
 phi_0=0.25; %uhlová poloha
-dphi_0=0; %uhlová rıchlos
+omega_0=0; %uhlová rıchlos
 
 %%
 simout=sim('cv_3_sim'); %spustíme simuláciu
@@ -23,11 +23,12 @@ T_sim=simout.tout;
 
 figure(1);
 hold on;
-plot(T_sim,phi_nonlinear,'-k','LineWidth',2);
-plot(T_sim,phi_linear,'-.k','LineWidth',2);
+plot(T_sim,phi_nonlinear,'-k','LineWidth',2); %vykreslíme signál uhlovej polohy nelineárneho modelu
+plot(T_sim,phi_linear,'-.k','LineWidth',2); %vykreslíme signál uhlovej polohy lineárneho modelu
 title('Priebeh vıchylky kyvadla') 
 xlabel('èas[s]')  %oznacime si osi grafov
 ylabel('\phi [rad]')
+legend('Nelineárny model kyvadla','Linearizovanı model kyvadla'),
 grid on
 
 
