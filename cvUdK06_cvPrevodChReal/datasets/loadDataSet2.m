c89c081883498f8d7fa76cd1586dc7981757e0b7
Martin Dodek <dodekm1@gmail.com>
2020-10-29 12:52:52 +0100

    cv6

---
+++ cvUdK06_cvPrevodChReal/datasets/loadDataSet2.m
@@ -0,0 +1,29 @@
+clear all;
+load('dataset2.mat')
+set(groot,'defaultLineLineWidth',1.5)
+figure(1)
+subplot(4,1,1)
+plot(ds2_ventilator(:,1), ds2_ventilator(:,2),'-k')
+xlabel('time[s]')
+ylabel('u_{ventilator} [ ]')
+title('Nastavenie ventilátora')
+grid on;
+subplot(4,1,2)
+plot(ds2_spirala(:,1), ds2_spirala(:,2),'-k')
+xlabel('time [s]')
+ylabel('u_{spirala} [ ]')
+title('Vstupný výkon špirály')
+grid on;
+subplot(4,1,3)
+plot(ds2_snimac_1(:,1), ds2_snimac_1(:,2),'-k')
+ylabel('T_1 [°C]')
+xlabel('time [s]')
+title('Meraná teplota 1')
+grid on;
+subplot(4,1,4)
+plot(ds2_snimac_2(:,1), ds2_snimac_2(:,2),'-k')
+xlabel('time[s]')
+ylabel('T_2 [°C]')
+title('Meraná teplota 2')
+grid on;
+set(gcf,'position',[50,50,1500,900])
---
+++ cvUdK06_cvPrevodChReal/datasets/loadDataSet4.m
@@ -0,0 +1,29 @@
+clear all;
+load('dataset4.mat')
+set(groot,'defaultLineLineWidth',1.5)
+figure(1)
+subplot(4,1,1)
+plot(ds4_ventilator(:,1), ds4_ventilator(:,2),'-k')
+xlabel('time[s]')
+ylabel('u_{ventilator} [ ]')
+title('Nastavenie ventilátora')
+grid on;
+subplot(4,1,2)
+plot(ds4_spirala(:,1), ds4_spirala(:,2),'-k')
+xlabel('time [s]')
+ylabel('u_{spirala} [ ]')
+title('Vstupný výkon špirály')
+grid on;
+subplot(4,1,3)
+plot(ds4_snimac_1(:,1), ds4_snimac_1(:,2),'-k')
+ylabel('T_1 [°C]')
+xlabel('time [s]')
+title('Meraná teplota 1')
+grid on;
+subplot(4,1,4)
+plot(ds4_snimac_2(:,1), ds4_snimac_2(:,2),'-k')
+xlabel('time[s]')
+ylabel('T_2 [°C]')
+title('Meraná teplota 2')
+grid on;
+set(gcf,'position',[50,50,1500,900])
