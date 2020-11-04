31f24df117cb9e813d8d6d42e856bbab62c3042c
Martin Dodek <dodekm1@gmail.com>
2020-11-04 10:20:09 +0100

    zadanie 2

--- cvUdK06_cvPrevodChReal/datasets/loadDataSet1.m
+++ cvUdK06_cvPrevodChReal/datasets/loadDataSet1.m
@@ -27,5 +27,38 @@ ylabel('T_2 [ ]')
 title('Meraná teplota 2')
 grid on;
 set(gcf,'position',[50,50,1500,900])
+%%
+idx_vent=find([diff(ds1_ventilator(:,2));false]);
 
+idx=diff(ds1_spirala(:,2))~=0;
+idx=[idx;false];
+idx_1=find(idx(1:idx_vent));
+idx_2=find(idx(idx_vent+1:end))+idx_vent;
+
+u_prevod_1=ds1_spirala(idx_1,2);
+T_prevod_1=ds1_snimac_1(idx_1,2);
+
+u_prevod_2=ds1_spirala(idx_2,2);
+T_prevod_2=ds1_snimac_1(idx_2,2);
+
+subplot(4,1,2)
+hold on
+plot(ds1_spirala(idx,1), ds1_spirala(idx,2),'+r')
+
+subplot(4,1,3)
+hold on
+plot(ds1_snimac_1(idx,1), ds1_snimac_1(idx,2),'+r')
+
+subplot(4,1,4)
+hold on
+plot(ds1_snimac_2(idx,1), ds1_snimac_2(idx,2),'+r')
+
+figure(2);
+hold on
+plot(u_prevod_1,T_prevod_1,'+k');
+plot(u_prevod_2,T_prevod_2,'ok');
+grid on;
+xlabel('u_{spirala} [ ]')
+ylabel('T [ ]')
+title('Prevodová charakteristika');
 
