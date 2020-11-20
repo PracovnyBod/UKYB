8b2276a843984203e8a63dcf80c7729d78312ea6
Martin Dodek <dodekm1@gmail.com>
2020-11-20 13:12:27 +0100

    GPC BMM

--- cvUdK05_cvPrevodCh_zadanie/sim/prev_char.m
+++ cvUdK05_cvPrevodCh_zadanie/sim/prev_char.m
@@ -23,27 +23,34 @@ simout=sim('prev_char_sim'); %spustíme simuláciu
 phi=simout.phi;
 time=simout.tout;
 idx=T_step/Ts*(1:length(u_prevod));
-
 phi_prevod=zeros(size(idx));
 window_size=20;
 
 figure(1)
-plot(time,phi,'-k','LineWidth',1);
+yyaxis right
+stairs((0:length(u_prevod)-1)*T_step,u_prevod,'--','LineWidth',1);
+ylabel('u [kg m^2 s^{-2}]')
+
+yyaxis left
+plot(time,phi,'-','LineWidth',1);
 grid on;
-xlabel('t [s]')
-ylabel('\phi [rad]')
+xlabel('t [s]');
+ylabel('\phi [rad]');
 hold on
 for i=1:length(idx)
  phi_prevod(i)=mean(phi(idx(i)-window_size+1:idx(i)));
  plot(time(idx(i)-window_size+1:idx(i)),ones(window_size,1)*phi_prevod(i),'-r','LineWidth',2)
-end    
+end
+title('Odozva systému na vstupný signál');
+legend('\phi','\phi_{mean}');
+
 figure(2)
 hold on
-plot(u_prevod,phi_prevod,'+r','LineWidth',2);
-plot(u_prevod,phi_prevod,'-k','LineWidth',2);
-
+plot(u_prevod,phi_prevod,'+r','LineWidth',1.5);
+plot(u_prevod,asin(u_prevod/(m*g*l)),'og','LineWidth',1.5);
+legend('zmeraná','analyticky')
 grid on;
-xlabel('u [ ]')
+xlabel('u [kg m^2 s^{-2}]')
 ylabel('\phi [rad]')
 title('Prevodová charakteristika');
 
