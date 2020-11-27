bcb64129a5ae27ad05b4e2eb15c839d0d3bb34b1
Martin Dodek <dodekm1@gmail.com>
2020-11-27 08:04:50 +0100

    cv9

--- cvUdK09_PID/sim/cv9.m
+++ cvUdK09_PID/sim/cv9.m
@@ -7,14 +7,19 @@ K=2;
 T=5;
 
 P=2; 
-I=1;
-D=1.5;
+I=0;
+D=0;
+
 N=5;
 
 %%
+use_filt=false;
 F_s=tf(K,[T,1]);
-%F_reg=tf(P,1)+tf(I,[1,0])+tf([D*N,0],[1,N]); %filtrovaná derivácia
+if use_filt
+F_reg=tf(P,1)+tf(I,[1,0])+tf([D*N,0],[1,N]); %filtrovaná derivácia
+else
 F_reg=tf(P,1)+tf(I,[1,0])+tf([D,0],1); %nefiltrovaná derivácia
+end
 F_oro=F_s*F_reg;
 F_uro=feedback(F_oro,1);
 %%
@@ -36,13 +41,13 @@ ylabel('u');
 legend('y','w','u');
 
 
-%%
+figure(2)
 pzplot(F_uro,'r')
 grid on;
 a = findobj(gca,'type','line')
 for i = 1:length(a)
-    set(a(i),'markersize',7) %change marker size
-    set(a(i), 'linewidth',2.0)  %change linewidth
+    set(a(i),'markersize',7) 
+    set(a(i), 'linewidth',2.0) 
 end
 set(gcf,'position',[0,0,550,500])
 
