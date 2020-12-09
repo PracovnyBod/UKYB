85175f13437892a59276b6ae04a04cffae1fb9e4
Martin Dodek <dodekm1@gmail.com>
2020-12-09 13:41:21 +0100

    CV 10

--- cvUdK010_LeadLag/sim/cv10.m
+++ cvUdK010_LeadLag/sim/cv10.m
@@ -1,12 +1,10 @@
 
-%%
 close all
 Tsim=20;
 Ts=0.1;
 
 figure(1);
-F=zpk([],[-2,-1,-0.5],100);
-
+F=zpk([],[-1,-2,-3],100);
 
 epsilon=0.05;
 
@@ -18,13 +16,13 @@ grid on;
 hold on;
 
 phi_z=pi/4;
-phi_0= -0.2618;
+phi_0= deg2rad(-15);
 
 phi_m=phi_z-phi_0;
 a=(1+sin(phi_m))/(1-sin(phi_m));
 
 d=-10*log10(a);
-omega_m=3.8;
+omega_m=6.85;
 T=1/(omega_m*sqrt(a));
 
 C=tf([a*T,1],[T,1]);
@@ -38,4 +36,5 @@ legend('Systém','Korekèný èlen','Otvorený regulaèný obvod')
 figure(2);
 F_uro=feedback(F_ORO,1);
 step(F_uro);
+
 grid on;
